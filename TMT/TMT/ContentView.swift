bd9fe4b40cdbfab859e5aa6deecdcaca8f99cdf7
dbqls200 <dbqls200@gmail.com>
2024-09-29 17:16:25 +0900

    feat: Initial Xcode project setup

---
+++ TMT/TMT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TMT
+//
+//  Created by 김유빈 on 9/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TMT/TMT/TMTApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMTApp.swift
+//  TMT
+//
+//  Created by 김유빈 on 9/29/24.
+//
+
+import SwiftUI
+
+@main
+struct TMTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
