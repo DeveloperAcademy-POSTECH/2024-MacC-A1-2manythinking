9b3e497476b2ff849635a5ec69c718a8c005fe94
dbqls200 <dbqls200@gmail.com>
2024-10-08 12:06:33 +0900

    chore: Add placeholder files to retain folder structure
    - 이전 커밋에서 빈 폴더만 추가했었음
    - git이 기본적으로 비어 있는 폴더는 추적하지 않음
    - 폴더링 구조 유지를 위해 임시 파일 추가함

---
+++ TMT/TMT/BusJourney/BusJourneyView.swift
@@ -0,0 +1,18 @@
+//
+//  BusJourneyView.swift
+//  TMT
+//
+//  Created by 김유빈 on 10/8/24.
+//
+
+import SwiftUI
+
+struct BusJourneyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BusJourneyView()
+}
