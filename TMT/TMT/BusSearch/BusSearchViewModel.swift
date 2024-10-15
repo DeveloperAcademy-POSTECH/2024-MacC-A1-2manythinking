04cda6fd2396256138fe1ec63677042e046e6f5d
dbqls200 <dbqls200@gmail.com>
2024-10-15 21:22:42 +0900

    feat: #10 Add bus number search functionality using CSV data
    - 사용자가 입력한 버스 번호에 대한 데이터만 필터링하는 작업

---
+++ TMT/TMT/BusSearch/BusSearchViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  BusSearchViewModel.swift
+//  TMT
+//
+//  Created by 김유빈 on 10/15/24.
+//
+
+import Foundation
+
+final class BusStopSearchViewModel: ObservableObject {
+    @Published var allBusStops: [BusStopInfo] = []
+    @Published var busStops: [BusStopInfo] = []
+
+    init() {
+        loadCSV()
+    }
+
+    private func loadCSV() {
+        Task {
+            guard let filepath = Bundle.main.path(forResource: "BusStopData", ofType: "csv") else {
+                print("Error \(#function) in \(#file) :: CSV file not found")
+                return
+            }
+            do {
+                let content = try String(contentsOfFile: filepath)
+
+                let response = content.components(separatedBy: "\n")
+
+                let searchResponse = response.map { $0.components(separatedBy: ",")}
+
+                await self.apply(searchResponse)
+
+            } catch {
+                print("Error \(#function) in \(#file)")
+            }
+        }
+    }
+
+    @MainActor
+    private func apply(_ searchResponse: [[String]]) {
+        for response in searchResponse {
+            self.allBusStops.append(BusStopInfo(busNumber: response[0],
+                                                busType: response[1],
+                                                stopOrder: response[2],
+                                                stopName: response[3],
+                                                romanizedStopName: response[4],
+                                                xCoordinate: response[5],
+                                                yCoordinate: String(response[6].dropLast(1))))
+        }
+    }
+
+    func searchBus(by number: String) {
+        busStops = allBusStops.filter { $0.busNumber.contains(number) }
+    }
+
+}
