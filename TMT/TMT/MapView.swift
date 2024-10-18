b3fc4ef49b289d99bc6819f49b0ad8e8de3b4a26
Minkyeong-Choi <minkyeong.choi@outlook.com>
2024-10-18 20:00:18 +0900

    fix: #12 delete some comments

--- TMT/TMT/MapView.swift
+++ TMT/TMT/MapView.swift
@@ -12,8 +12,6 @@ struct MapView: View {
     @StateObject private var locationManager = LocationManager()
     @StateObject var busStopSearchViewModel = BusStopSearchViewModel()
     
-    // TODO: 사용자 위치 바뀌어도 화면 다시 업데이트 안되게 하기.
-    // 투두: 다시 테스트 해보고, 밑에 스트링 값 없을 때 대신 넣는거 생각해보기.
     var body: some View {
         ZStack {
             Map(coordinateRegion: $locationManager.region, showsUserLocation: true, annotationItems: busStopSearchViewModel.nameAndCoordinates) { stop in
