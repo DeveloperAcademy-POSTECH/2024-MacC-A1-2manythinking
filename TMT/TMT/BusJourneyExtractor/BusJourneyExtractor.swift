304c9bcdab86a113da6edc127e613e0630f7fb56
Minkyeong-Choi <minkyeong.choi@outlook.com>
2024-10-26 19:03:23 +0900

    feat: #30 Add logic on slicing the real route

--- TMT/TMT/BusJourneyExtractor/BusJourneyExtractor.swift
+++ TMT/TMT/BusJourneyExtractor/BusJourneyExtractor.swift
@@ -17,38 +17,48 @@ class BusJourneyExtractor {
         var busNumber: String = ""
         var busStopToGetOff: String = ""
         
-        // 환승 노선 정보
-        var busStopToBoard2: String = ""
-        var busNumber2: String = ""
-        var busStopToGetOff2: String = ""
-        var busStopToBoard3: String = ""
-        var busNumber3: String = ""
-        var busStopToGetOff3: String = ""
-        
         let busColors = ["Blue", "General", "Express", "Green", "Town"]
         let busNumbers = ["206", "207", "209", "216", "302", "305", "306", "308", "600", "700", "800", "900"]
         let arrivalInfoWords = ["min", "stops", "Interval:", "Weekdays", "Total", "buses", "Everyday", "Weekends", "No", "ETA"]
         
-        let stringInArray = recognizedString.components(separatedBy: CharacterSet.whitespacesAndNewlines)
+        var stringInArray = recognizedString.components(separatedBy: CharacterSet.whitespacesAndNewlines)
+        let firstMinIndex = stringInArray.enumerated().filter({ $0.element.contains("min") }).first?.offset ?? 0
+        stringInArray.remove(at: firstMinIndex)
         
-        // 실제 사용자가 이용할 경로에 대한 문자열만 남깁니다.
-        if let secondMinIndex = stringInArray.enumerated().filter({ $0.element.contains("min") }).dropFirst().first?.offset,
-           let goIndex = stringInArray.firstIndex(of: "GO") {
-            if secondMinIndex < goIndex {
-                slicedArray = Array(stringInArray[secondMinIndex...goIndex])
-            } else {
-                print("두 번째 'min'이 'Go'보다 뒤에 있습니다.")
-                guard let secondGoIndex = stringInArray.enumerated().filter({ $0.element == "GO" }).dropFirst().first?.offset else { return nil }
-                if secondMinIndex < secondGoIndex {
-                    slicedArray = Array(stringInArray[secondMinIndex...secondGoIndex])
+        // 사용자가 이용할 버스 루트만 슬라이스합니다.
+        var currentMinIndex = 0
+        while slicedArray.count < 15 {
+            if let nextMinIndex = stringInArray.enumerated()
+                .dropFirst(currentMinIndex)
+                .filter({ $0.element.contains("min") })
+                .first?.offset,
+               let goIndex = stringInArray.firstIndex(of: "GO") {
+                if nextMinIndex < goIndex {
+                    slicedArray = Array(stringInArray[nextMinIndex...goIndex])
+                    currentMinIndex = nextMinIndex + 1
+                } else {
+                    print("다음 'min'이 'GO' 뒤에 있습니다.")
+                    guard let secondGoIndex = stringInArray.enumerated()
+                        .dropFirst(currentMinIndex)
+                        .filter({ $0.element == "GO" })
+                        .dropFirst()
+                        .first?.offset else { break }
+                    
+                    if nextMinIndex < secondGoIndex {
+                        slicedArray = Array(stringInArray[nextMinIndex...secondGoIndex])
+                        currentMinIndex = nextMinIndex + 1
+                    }
                 }
+            } else {
+                print("더 이상 'min'을 찾을 수 없습니다.")
+                break
             }
-        } else {
-            print("Slicing failed.")
         }
         
         // OCR 문제를 막기 위해 경로에서 분 정보를 제거합니다. ex) 3m, 12m
-        let filteredArray = slicedArray.filter { !$0.matches("[0-9]+m") && $0 != "Best" && $0 != "route" }
+        let filteredArray = slicedArray.filter { !$0.matches("\\b\\d+m\\b(?!in)") && $0 != "Best" && $0 != "route" }
+        print("Sliced array: \(slicedArray)")
+        print("filterd array: \(filteredArray)")
         
         // 어떤 종류의 버스를 타는지 구합니다. 예시: ["blue", "blue", "general"]
         sortOfBuses = filteredArray.filter { busColors.contains($0) }
@@ -74,11 +84,39 @@ class BusJourneyExtractor {
                     if sortOfBusNumber.count < 2 {
                         // 버스 한대만 감.
                         print("버스 한대만 감")
+                        
+                        // 버스 번호를 구합니다.
+                        if !sortOfBusNumber[0].isEmpty {
+                            busNumber = sortOfBusNumber.first ?? ""
+                        } else {
+                            print("No bus info.")
+                        }
+                        
+                        // 탑승 정류장을 구합니다.
+                        if let firstIndex = filteredArray.firstIndex(of: sortOfBuses[0]),
+                           let secondIndex = filteredArray.firstIndex(where: { $0 == "ETA" || $0 == busNumber }),
+                           firstIndex < secondIndex {
+                            let result = Array(filteredArray[(firstIndex + 1)..<(secondIndex)].dropLast())
+                            busStopToBoard = stringArrayToStirng(stringArray: result)
+                        } else {
+                            print("Failed to extract busStopBoard.")
+                        }
+                        
+                        // 하차 정류장을 구합니다.
+                        if let firstIndex = filteredArray.firstIndex(of: "off"),
+                           let secondIndex = filteredArray.firstIndex(of: "GO"),
+                           firstIndex < secondIndex {
+                            let result = Array(filteredArray[(firstIndex + 1)..<secondIndex].dropLast())
+                            busStopToGetOff = stringArrayToStirng(stringArray: result)
+                        } else {
+                            print("Failed to extract busStopToGetOff.")
+                        }
+                        
                     } else {
                         // 버스 여러대 감.
                         print("버스 여러대 감")
                     }
-                
+                    
                 } else {
                     // 환승 노선
                     // TODO: 환승 노선은 다음 이터레이션에 구현될 예정입니다.
@@ -99,7 +137,7 @@ class BusJourneyExtractor {
                         }
                         print("busStopToBoard: \(busStopToBoard)")
                     } else {
-                        print("'off'와 '306(기본)' 사이의 값을 찾을 수 없습니다.")
+                        print("Failed to get busStopToBoard")
                     }
                     
                     // 첫번째 노선 탑승할 버스 번호 정보
@@ -149,7 +187,6 @@ class BusJourneyExtractor {
                     } else {
                         // 버스 여러대 감.
                         print("버스 여러대 감")
-                        
                     }
                 } else {
                     // 환승노선
@@ -158,7 +195,7 @@ class BusJourneyExtractor {
                 }
             }
         }
-    
+        
         return busStopToBoard + "," + busNumber + "," + busStopToGetOff
     }
     
@@ -175,7 +212,7 @@ class BusJourneyExtractor {
         }
     }
 }
-    
+
 extension String {
     func matches(_ pattern: String) -> Bool {
         return self.range(of: pattern, options: .regularExpression) != nil
