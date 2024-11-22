224079ddb00ed98d14477b1597d242119e2d1983
Minkyeong-Choi <minkyeong.choi@outlook.com>
2024-11-22 23:57:52 +0900

    fix: #105: Fix Reupload and Start button in ScannedJourneyInfoView

--- TMT/TMT/BusJourneyScanned/Views/ScannedJourneyInfoView.swift
+++ TMT/TMT/BusJourneyScanned/Views/ScannedJourneyInfoView.swift
@@ -38,14 +38,12 @@ struct ScannedJourneyInfoView: View {
         ZStack {
             Color.white
                 .ignoresSafeArea()
-            ScrollView {
-                VStack {
+            VStack {
+                ScrollView {
                     if !imageHandler.showAlertScreen {
-                        UploadedPhotoView(selectedImage: $imageHandler.selectedImage)
-                            .padding(.horizontal, 16)
+                        UploadedPhotoView(selectedImage: $imageHandler.selectedImage) 
                     } else {
                         UploadedPhotoView(selectedImage: .constant(nil))
-                            .padding(.horizontal, 16)
                     }
                     
                     VStack(alignment: .leading) {
@@ -63,91 +61,90 @@ struct ScannedJourneyInfoView: View {
                             }
                             .foregroundStyle(.red600)
                         }
-                        
-                        HStack(spacing: 0) {
-                            Button {
-                                showingAlert = true
-                            } label: {
-                                ZStack {
-                                    if !imageHandler.showAlertText {
-                                        RoundedRectangle(cornerRadius: 8)
-                                            .stroke(Color.Brand.primary, lineWidth: 1)
-                                        Text("Reupload")
-                                            .foregroundStyle(.brandPrimary)
-                                    } else {
-                                        RoundedRectangle(cornerRadius: 8)
-                                            .fill(.grey900)
-                                            .stroke(.grey900)
-                                        Text("Reupload")
-                                            .foregroundStyle(.white)
-                                    }
-                                }
-                            }
-                            .padding(.trailing, 8)
-                            .alert("Information will disappear.", isPresented: $showingAlert) {
-                                Button {
-                                    showingAlert = false
-                                } label: {
-                                    Text("Cancel")
-                                        .foregroundStyle(.blue)
-                                }
-                                
-                                Button {
-                                    showingAlert = false
-                                    showingPhotosPicker = true
-                                    imageHandler.scannedJourneyInfo = ScannedJourneyInfo(busNumber: "", startStop: "", endStop: "")
-                                    imageHandler.selectedImage = nil
-                                } label: {
-                                    Text("Confirm")
-                                        .foregroundStyle(.blue)
-                                        .fontWeight(.bold)
-                                }
-                            } message: {
-                                Text("The previously uploaded image information will disappear. Do you want to proceed?")
-                            }
-                            
-                            PhotosPicker(selection: $pickedItem, matching: .screenshots) {
-                                EmptyView()
-                            }
-                            .onChange(of: pickedItem) {
-                                imageHandler.loadImage(from: pickedItem, viewCategory: "ScannedJourneyInfoView", completion: {})
-                            }
-                            .photosPicker(isPresented: $showingPhotosPicker, selection: $pickedItem, matching: .screenshots)
-                            
-                            NavigationLink(destination: BusStopView(path: $path)
-                                .environmentObject(locationManager)
-                                .environmentObject(searchModel)
-                                .environmentObject(activityManager)
-                                .environmentObject(journeyModel)
-                                .environmentObject(imageHandler), tag: 1, selection: $tag) {
-                                    EmptyView()
-                                }
-                            
-                            FilledButton(title: "Start", fillColor: imageHandler.showAlertText ? .grey100 : .brandPrimary) {
-                                if !imageHandler.showAlertText {
-                                    journeyModel.setJourneyStops(
-                                        busNumberString: imageHandler.scannedJourneyInfo.busNumber,
-                                        startStopString: imageHandler.scannedJourneyInfo.startStop,
-                                        endStopString: imageHandler.scannedJourneyInfo.endStop
-                                    )
-                                    
-                                    guard let endStop = journeyModel.journeyStops.last else { return }
-                                    activityManager.startLiveActivity(
-                                        destinationInfo: endStop,
-                                        remainingStops: locationManager.remainingStops
-                                    )
-                                    tag = 1
-                                    path.append("BusStop")
-                                }
-                            }
-                            .disabled(imageHandler.showAlertText)
-                        }
-                        .frame(height: 52)
-                        .padding(.vertical, 12.5)
                     }
-                    .padding(.horizontal, 16)
                 }
+                HStack(spacing: 0) {
+                    Button {
+                        showingAlert = true
+                    } label: {
+                        ZStack {
+                            if !imageHandler.showAlertText {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .stroke(Color.Brand.primary, lineWidth: 1)
+                                Text("Reupload")
+                                    .foregroundStyle(.brandPrimary)
+                            } else {
+                                RoundedRectangle(cornerRadius: 8)
+                                    .fill(.grey900)
+                                    .stroke(.grey900)
+                                Text("Reupload")
+                                    .foregroundStyle(.white)
+                            }
+                        }
+                    }
+                    .padding(.trailing, 8)
+                    .alert("Information will disappear.", isPresented: $showingAlert) {
+                        Button {
+                            showingAlert = false
+                        } label: {
+                            Text("Cancel")
+                                .foregroundStyle(.blue)
+                        }
+                        
+                        Button {
+                            showingAlert = false
+                            showingPhotosPicker = true
+                            imageHandler.scannedJourneyInfo = ScannedJourneyInfo(busNumber: "", startStop: "", endStop: "")
+                            imageHandler.selectedImage = nil
+                        } label: {
+                            Text("Confirm")
+                                .foregroundStyle(.blue)
+                                .fontWeight(.bold)
+                        }
+                    } message: {
+                        Text("The previously uploaded image information will disappear. Do you want to proceed?")
+                    }
+                    
+                    PhotosPicker(selection: $pickedItem, matching: .screenshots) {
+                        EmptyView()
+                    }
+                    .onChange(of: pickedItem) {
+                        imageHandler.loadImage(from: pickedItem, viewCategory: "ScannedJourneyInfoView", completion: {})
+                    }
+                    .photosPicker(isPresented: $showingPhotosPicker, selection: $pickedItem, matching: .screenshots)
+                    
+                    NavigationLink(destination: BusStopView(path: $path)
+                        .environmentObject(locationManager)
+                        .environmentObject(searchModel)
+                        .environmentObject(activityManager)
+                        .environmentObject(journeyModel)
+                        .environmentObject(imageHandler), tag: 1, selection: $tag) {
+                            EmptyView()
+                        }
+                    
+                    FilledButton(title: "Start", fillColor: imageHandler.showAlertText ? .grey100 : .brandPrimary) {
+                        if !imageHandler.showAlertText {
+                            journeyModel.setJourneyStops(
+                                busNumberString: imageHandler.scannedJourneyInfo.busNumber,
+                                startStopString: imageHandler.scannedJourneyInfo.startStop,
+                                endStopString: imageHandler.scannedJourneyInfo.endStop
+                            )
+                            
+                            guard let endStop = journeyModel.journeyStops.last else { return }
+                            activityManager.startLiveActivity(
+                                destinationInfo: endStop,
+                                remainingStops: locationManager.remainingStops
+                            )
+                            tag = 1
+                            path.append("BusStop")
+                        }
+                    }
+                    .disabled(imageHandler.showAlertText)
+                }
+                .frame(height: 52)
+                .padding(.vertical, 12.5)
             }
+            .padding(.horizontal, 16)
             
             if isShowingInformation {
                 InformationModalView(isShowingInformation: $isShowingInformation)
