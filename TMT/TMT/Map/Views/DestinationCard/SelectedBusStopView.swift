173d42954177140894a8e99bf8b29ee2a0199255
dbqls200 <dbqls200@gmail.com>
2024-11-24 00:29:48 +0900

    refactor: #99 Reorganize view files in Map/Views folder into separate folders for better structure

---
+++ TMT/TMT/Map/Views/DestinationCard/SelectedBusStopView.swift
@@ -0,0 +1,64 @@
+//
+//  SelectedBusStopView.swift
+//  TMT
+//
+//  Created by Choi Minkyeong on 11/19/24.
+//
+
+import SwiftUI
+
+struct SelectedBusStopView: View {
+    @EnvironmentObject var selectedStopManager: SelectedStopManager
+    @EnvironmentObject var searchModel: BusSearchModel
+    @State private var stopOrder: Int? = 0
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                Button {
+                    selectedStopManager.isTapped = false
+                } label: {
+                    Image(systemName: "xmark.circle.fill")
+                        .foregroundStyle(Color(red: 0.5, green: 0.5, blue: 0.5).opacity(0.5))
+                        .font(.system(size: 30))
+                }
+            }
+            if let stopOrder = stopOrder {
+                BusStopDetailView(stopNameKorean: searchModel.filteredBusDataForNumber[stopOrder].stopNameKorean ?? "", stopNameRomanized: searchModel.filteredBusDataForNumber[stopOrder].stopNameRomanized ?? "", stopNameNaver: searchModel.filteredBusDataForNumber[stopOrder].stopNameNaver ?? "")
+            } else {
+                Text("Error is occured. Please try again.")
+            }
+        }
+        .padding([.horizontal, .bottom], 32)
+        .padding(.top, 12)
+        .background {
+            Rectangle()
+                .foregroundStyle(.basicWhite)
+                .frame(maxWidth: .infinity)
+                .ignoresSafeArea()
+        }
+        .onAppear(perform: updateStateOrder)
+        .onChange(of: selectedStopManager.selectedIndex) {
+            updateStateOrder()
+        }
+    }
+    
+    private func updateStateOrder() {
+        stopOrder = selectedStopManager.selectedIndex
+    }
+}
+
+#Preview {
+    let selectedStopManager = SelectedStopManager()
+    let searchModel = BusSearchModel()
+    
+    searchModel.filteredBusDataForNumber = BusStop.busStopDummy
+    
+    selectedStopManager.selectedIndex = 3
+    selectedStopManager.isTapped = true
+
+    return SelectedBusStopView()
+        .environmentObject(selectedStopManager)
+        .environmentObject(searchModel)
+}
