bea7a28107d74645110cc75414447e29bac84f91
Minkyeong-Choi <minkyeong.choi@outlook.com>
2024-12-01 19:07:46 +0900

    feat: #131 Modify Coordinate struct

--- TMT/TMT/BusSearch/Models/Coordinate.swift
+++ TMT/TMT/BusSearch/Models/Coordinate.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct Coordinate: Identifiable {
     var id = UUID()
+    var busNumber: String
+    var stopNameKorean: String
+    var stopOrder: Int
     var latitude: Double
     var longitude: Double
 }
